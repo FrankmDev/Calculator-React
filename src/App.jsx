5fe4be94aa8d427d7f28f8afbe55a5b2e2b21037
FrankmDev <frankmmarindev@gmail.com>
2024-01-19 21:35:32 +0100

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,53 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-red-500 font-bold ">Hello World</h1>
-    </>
+    <section className="container rounded-xl overflow-hidden p-5 relative">
+      <div className="display">
+        <div className="input">0</div>
+        <br />
+        <div className="output">0</div>
+      </div>
+      <br />
+      <div className="calculator">
+        <button className="btn-number" value="7">
+          7
+        </button>
+        <button className="btn-number" value="8">
+          8
+        </button>
+        <button className="btn-number" value="9">
+          9
+        </button>
+        <button className="btn-op">*</button>
+        <br />
+        <button className="btn-number" value="4">
+          4
+        </button>
+        <button className="btn-number" value="5">
+          5
+        </button>
+        <button className="btn-number" value="6">
+          6
+        </button>
+        <button className="btn-op">/</button>
+        <br />
+        <button className="btn-number" value="1">
+          1
+        </button>
+        <button className="btn-number" value="2">
+          2
+        </button>
+        <button className="btn-number" value="3">
+          3
+        </button>
+        <button className="btn-op">-</button>
+        <br />
+        <button className="btn-reload">C</button>
+        <button className="btn-number">0</button>
+        <button className="btn-equal">=</button>
+        <button className="btn-op">+</button>
+      </div>
+    </section>
   );
 }
 
