baa8bd7558f38cbc0ffd50913dc54ae103cba6b1
FrankmDev <frankmmarindev@gmail.com>
2024-01-19 22:48:19 +0100

    Calculator

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,65 @@ import "./App.css";
 function App() {
   return (
     <section className="container rounded-xl overflow-hidden p-5 relative">
-      <div className="display">
+      <div id="display">
         <div className="input">0</div>
-        <br />
         <div className="output">0</div>
       </div>
-      <br />
+      <button id="clear" className="btn-number" value="7" type="reset">
+        C
+      </button>
       <div className="calculator">
-        <button className="btn-number" value="7">
+        <button id="seven" className="btn-number" value="7">
           7
         </button>
-        <button className="btn-number" value="8">
+        <button id="eight" className="btn-number" value="8">
           8
         </button>
-        <button className="btn-number" value="9">
+        <button id="nine" className="btn-number" value="9">
           9
         </button>
-        <button className="btn-op">*</button>
+        <button id="multiply" className="btn-op">
+          *
+        </button>
         <br />
-        <button className="btn-number" value="4">
+        <button id="four" className="btn-number" value="4">
           4
         </button>
-        <button className="btn-number" value="5">
+        <button id="five" className="btn-number" value="5">
           5
         </button>
-        <button className="btn-number" value="6">
+        <button id="six" className="btn-number" value="6">
           6
         </button>
-        <button className="btn-op">/</button>
+        <button id="divide" className="btn-op">
+          /
+        </button>
         <br />
-        <button className="btn-number" value="1">
+        <button id="one" className="btn-number" value="1">
           1
         </button>
-        <button className="btn-number" value="2">
+        <button id="two" className="btn-number" value="2">
           2
         </button>
-        <button className="btn-number" value="3">
+        <button id="three" className="btn-number" value="3">
           3
         </button>
-        <button className="btn-op">-</button>
+        <button id="subtract" className="btn-op">
+          -
+        </button>
         <br />
-        <button className="btn-reload">C</button>
-        <button className="btn-number">0</button>
-        <button className="btn-equal">=</button>
-        <button className="btn-op">+</button>
+        <button id="decimal" className="btn-reload">
+          .
+        </button>
+        <button id="zero" className="btn-number">
+          0
+        </button>
+        <button id="equals" className="btn-equal">
+          =
+        </button>
+        <button id="add" className="btn-op">
+          +
+        </button>
       </div>
     </section>
   );
